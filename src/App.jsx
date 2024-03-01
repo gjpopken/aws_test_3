9e5ddeba531d3ee159f233ad4455845efd1bf9a1
Gavin Popken <gavinpopkenart@gmail.com>
2024-03-01 17:13:35 -0600

    set up to create a multipart formdata object

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [file, setFile] = useState(null)
+
+  const handleSubmit = () => {
+    if (file) {
+      // Creates a new object for storing different media types.
+      const formData = new FormData()
+      // Creates a new key-value pair. The first 'file' is the key, the other is the value.
+      formData.append('file', file)
+    }
+
+
+
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <p>{file}</p> */}
+      <input type="file" accept='audio/*,video/*,image/*' capture='user' onChange={(e) => setFile(e.target.value)} />
+      <button onClick={handleSubmit}>Submit</button>
     </>
   )
 }
