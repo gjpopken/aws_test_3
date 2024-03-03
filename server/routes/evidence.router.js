5692deee2271326c630dcda5357d655485c7bda4
Gavin Popken <gavinpopkenart@gmail.com>
2024-03-03 06:32:51 -0600

    the GET request now gets a signed URL

--- server/routes/evidence.router.js
+++ server/routes/evidence.router.js
@@ -8,6 +8,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 // Requiring in the AWS functionality.
 const aws = require('@aws-sdk/client-s3')
+const signer = require('@aws-sdk/s3-request-presigner')
 
 // ! References for AWS S3 Bucket
 //  All of this informatio comes from the AWS site for S3 buckets
@@ -30,8 +31,34 @@ const s3 = new aws.S3Client({
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     // GET route code here
+    const queryText = `
+    SELECT * FROM "evidence";
+    `
+    pool.query(queryText)
+        .then(async result => {
+            for (e of result.rows) {
+                // For each piece of evidence, I need to create a new
+                // GetObject command, which specifies the bucket I want to go into,
+                // as well as the specific file I want to get (the Key)
+                const command = new aws.GetObjectCommand({
+                    Bucket: bucketName,
+                    Key: e.file_url,
+                })
+                // the getSignedUrl method will send a unique and temporary URL that references the 
+                // specific image.
+                const url = await signer.getSignedUrl(s3, command, {expiresIn: 3600})
+
+                // I assign that AWS url to a new key, aws_url, in the specific evidence obj.
+                e.aws_url = url
+            }
+
+            res.send(result.rows)
+        }).catch(err => {
+            console.log('GET error with Pool: ', err);
+            res.sendStatus(500)
+        })
 });
 
 // --------------upload.single is the multer middleware
@@ -65,13 +92,13 @@ router.post('/', upload.single('file'), async (req, res) => {
         `
         const queryParams = [req.body.title, req.body.notes, req.file.originalname, req.body.user_id, req.file.mimetype]
         pool.query(queryText, queryParams)
-        .then(result => {
-            res.sendStatus(201)
-        }).catch(err => {
-            console.log("Error with Pool:", err);
-            res.sendStatus(500)
-        })
-        
+            .then(result => {
+                res.sendStatus(201)
+            }).catch(err => {
+                console.log("Error with Pool:", err);
+                res.sendStatus(500)
+            })
+
     } catch (error) {
         console.log('Here is the error from AWS: ', error);
         res.sendStatus(500)
